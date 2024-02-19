be72920565eb81fd6ee56cb3aa04403585bc6977
Student <elkadjk@.pl>
2024-02-19 12:13:37 +0100

    Poniedzialek19.02

--- src/wypozyczenie/Egzemplarz.java
+++ src/wypozyczenie/Egzemplarz.java
@@ -4,5 +4,27 @@ import ksiazka.Ksiazka;
 
 public class Egzemplarz {
     private Ksiazka ksiazka;
-    
+
+    public enum Stan{
+        WYPOZYCZONY,WOLNY
+    }
+    private Stan stan;
+
+    public Egzemplarz(Ksiazka ksiazka) {
+        this.ksiazka = ksiazka;
+        stan = Stan.WOLNY;
+    }
+
+    public Stan getStan() {
+        return stan;
+    }
+
+    public void setStan(Stan stan) {
+        this.stan = stan;
+    }
+
+    public Ksiazka getKsiazka() {
+        return ksiazka;
+    }
+
 }
--- src/wypozyczenie/Wypozyczenie.java
+++ src/wypozyczenie/Wypozyczenie.java
@@ -1,4 +1,25 @@
 package wypozyczenie;
 
+import java.time.LocalDate;
+
 public class Wypozyczenie {
+
+    private static final int DLUGOSC_WYPOZYCZENIA = 30;
+    private static final double KARA = 0.1;
+    private Egzemplarz egzemplarz;
+    private LocalDate dataWypozyczenia;
+    private LocalDate dataOddania;
+    private LocalDate przewidywanaDataOddanial;
+
+    public Wypozyczenie(Egzemplarz egzemplarz) {
+        this.egzemplarz = egzemplarz;
+        egzemplarz.setStan(Egzemplarz.Stan.WYPOZYCZONY);
+        dataWypozyczenia = LocalDate.now();
+        przewidywanaDataOddanial = dataWypozyczenia.plusDays(DLUGOSC_WYPOZYCZENIA);
+        dataOddania = null;
+
+    }
+    public double oddaj(){
+
+    }
 }
